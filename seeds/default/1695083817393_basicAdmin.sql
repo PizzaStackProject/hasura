a217bf67a606d44b04f8d06d39c61b14820b527e
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2023-09-19 02:37:39 +0200

    fix seed and add .env to gitignore

---
+++ seeds/default/1695083817393_basicAdmin.sql
@@ -0,0 +1,2 @@
+SET check_function_bodies = false;
+INSERT INTO public.admin (id, username, password) VALUES ('50728cd7-c1d2-47b9-b6eb-5672a22007ff', 'test', 'b0ab638e4c66d4d1f02b563e300acfb0e9f4ab93c8401f40b60033b942ce81db40955f559ddd0b472b6e1d42a1f905496a1d0ad347af70b4af461d52357a441c');

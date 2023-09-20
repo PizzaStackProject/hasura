cebbcdfe13f722b74e13a520a4a50aa73f8f86f3
Yaroslav Ustinov <y.ustinov2004@gmail.com>
2023-09-20 08:28:55 +0200

    added env variables and updated seeds

---
+++ seeds/default/1695188468981_myCategories.sql
@@ -0,0 +1,3 @@
+SET check_function_bodies = false;
+INSERT INTO public.category (id, title, slug) VALUES ('b790a7d3-5b87-47c6-b14c-8f32407797c2', 'Pizza', 'pizza');
+INSERT INTO public.category (id, title, slug) VALUES ('8094043e-b153-458e-a1ae-dd2795548445', 'Drink', 'drink');

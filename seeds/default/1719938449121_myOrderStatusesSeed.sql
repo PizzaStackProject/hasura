efb6139501db7e6bb4d342c01b50e4a9591ddd6c
Yaroslav Ustinov <69744663+Yaroslaw07@users.noreply.github.com>
2024-07-02 18:42:49 +0200

    Added seed for order_status

---
+++ seeds/default/1719938449121_myOrderStatusesSeed.sql
@@ -0,0 +1,6 @@
+SET check_function_bodies = false;
+INSERT INTO public.order_status (id, label) VALUES ('NEW', 'New');
+INSERT INTO public.order_status (id, label) VALUES ('IN_PROGRESS', 'In Progress');
+INSERT INTO public.order_status (id, label) VALUES ('DONE', 'Done');
+INSERT INTO public.order_status (id, label) VALUES ('CANCELED', 'Canceled');
+INSERT INTO public.order_status (id, label) VALUES ('DELIVERED', 'Devilered');
